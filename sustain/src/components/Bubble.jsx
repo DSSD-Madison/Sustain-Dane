cc04d88514823e1a66af65da29284aa66587fd7b
Noah Li <thenoahli06@gmail.com>
2026-02-27 15:44:47 -0600

    reduced size of bubbles in module.css

--- sustain/src/components/Bubble.jsx
+++ sustain/src/components/Bubble.jsx
@@ -1,15 +1,12 @@
 import styles from "./Bubble.module.css";
 
 export default function Bubble({ point }) {
+  const co2Avoided = point?.infos?.[0]?.co2 ?? 0;
 
-    const co2Avoided = point?.infos?.[0]?.co2 ?? 0;
-
-    return (
-        <div className={styles.bubble}>
-            <div className={styles.icon}>🌱</div>
-            <div className={styles.value}>
-                {co2Avoided} kg
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.bubble}>
+      <div className={styles.icon}>🌱</div>
+      <div className={styles.value}>{co2Avoided} kg</div>
+    </div>
+  );
 }
