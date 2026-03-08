2e618e1e10e06e2b9c50be008566e078ef7c2a6b
Felipe Basqueroto <basqueroto.felipe125@gmail.com>
2026-03-07 21:02:36 -0600

    layer 1 do heatmap

--- sustain/src/components/Bubble.jsx
+++ sustain/src/components/Bubble.jsx
@@ -4,8 +4,14 @@ export default function Bubble({ point }) {
 
     const co2Avoided = point?.infos?.[0]?.co2 ?? 0;
 
+    const size = Math.max(60, co2Avoided * 0.17);
+
     return (
-        <div className={styles.bubble}>
+        <div className={styles.bubble}
+             style={{
+                 width: `${size}px`,
+                 height: `${size}px`
+             }}>
             <div className={styles.icon}>🌱</div>
             <div className={styles.value}>
                 {co2Avoided} kg
