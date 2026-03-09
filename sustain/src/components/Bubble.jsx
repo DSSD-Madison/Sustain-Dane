292ae1d14f6f7ff8d505bf5c5c5126a246d8087e
Felipe Basqueroto <basqueroto.felipe125@gmail.com>
2026-03-08 19:45:10 -0500

    image

--- sustain/src/components/Bubble.jsx
+++ sustain/src/components/Bubble.jsx
@@ -4,7 +4,7 @@ export default function Bubble({ point }) {
 
     const co2Avoided = point?.infos?.[0]?.co2 ?? 0;
 
-    const size = Math.max(100, co2Avoided * 0.25);
+    const size = Math.max(100, co2Avoided * 0.23); // add a max
 
     return (
         <div
